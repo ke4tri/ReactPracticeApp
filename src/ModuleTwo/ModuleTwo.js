9d6de1f8b3f09b35183a165973faf869c60a760b
Wayne Collier <ke4tri@gmail.com>
2019-05-07 17:45:54 -0500

    created my own png logo for a better look

--- src/ModuleTwo/ModuleTwo.js
+++ src/ModuleTwo/ModuleTwo.js
@@ -14,23 +14,26 @@ render() {
     <Carousel className="carouselWrapper clip-polygon"showArrows={false} showStatus={false} showThumbs={false} autoPlay infiniteLoop>
     <div>
         <img src="https://www.wonolo.com/wp-content/uploads/2019/01/warehouse-operations.jpg" />
-        <p className="legend">TDelivery Driver</p>
+        <div className="imgNote">
+        <p className="display-3">Warehose Ops</p>
+        </div>
+       
     </div>
     <div>
         <img src="https://www.wonolo.com/wp-content/uploads/2019/01/delivery-drivers.jpg" />
-        <p className=".imgNote"></p>
+        <p className="legend imgNote">Delivery Drivers</p>
     </div>
     <div>
         <img src="https://www.wonolo.com/wp-content/uploads/2019/01/general-labor.jpg" />
-        <p className=".imgNote"></p>
+        <p className="legend imgNote display-4">General labor</p>
     </div>
     <div>
         <img src="https://www.wonolo.com/wp-content/uploads/2018/07/Homepage_Merchandising-1.jpg" />
-        <p className=".imgNote"></p>
+        <p className="legend imgNote">Merchandising</p>
     </div>
     <div>
         <img src="https://www.wonolo.com/wp-content/uploads/2018/05/Home_EventStaffing2_Placeholder.jpg" />
-        <p className=".imgNote"></p>
+        <p className="legend imgNote">EventStaffing</p>
     </div>
 </Carousel>
 
